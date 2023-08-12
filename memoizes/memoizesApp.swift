fcc152c791982c744c937c0b873d589761168200
Kennedy F. Silva <contact@kennedyribeiro.dev>
2023-08-12 17:03:29 -0300

    Initial Commit

---
+++ memoizes/memoizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoizesApp.swift
+//  memoizes
+//
+//  Created by Kennedy Ferreira da Silva Ribeiro on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct memoizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
